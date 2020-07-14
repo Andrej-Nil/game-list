d6d4fa8ac5356624f42ca4b062fb3f2ee757c560
Andrej-Nil <and.kucheroff@yandex.ru>
2020-07-14 13:48:26 +0500

    Певый коммит. Пустой проект. Установка note-scss, react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+
+const application = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      {application}
   </React.StrictMode>,
   document.getElementById('root')
 );
